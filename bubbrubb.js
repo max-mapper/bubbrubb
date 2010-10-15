fbdf50e25c57ea5dd7ca1b621b84ffdddcc03fd7
Max Ogden <max@maxogden.com>
2010-10-14 18:57:20 -0700

    making links do stuff

--- bubbrubb.js
+++ bubbrubb.js
@@ -17,7 +17,7 @@
 
       $.each(feedData, function(i, items) {
         $.each(items, function(i, row){
-          $('#bubbrubb > ul').append("<li class='message'><img class='avatar' src='http://www.gravatar.com/avatar/9ec4d6d86cbf793e1ff04ed4478d95ab.jpg?s=40&d=identicon'/><div class='content'><span class='from'><a class='hover_profile'>" + row.actor.displayName + "</a>	" + row.object.summary + "</span><div class='info'>" + row.postedTime + "</div></div></li>");
+          $('#bubbrubb > ul').append("<li class='message'><img class='avatar' src='http://www.gravatar.com/avatar/9ec4d6d86cbf793e1ff04ed4478d95ab.jpg?s=40&d=identicon'/><div class='content'><span class='from'><a href='" + row.object.permalinkUrl + "'class='hover_profile'>" + row.actor.displayName + "</a>	" + row.object.summary + "</span><div class='info'>" + row.postedTime + "</div></div></li>");
         });
       });
     }
