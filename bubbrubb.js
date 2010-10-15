09f9876d38c4871390a525063265212d83f99149
Whoahbot <whoahbot@gmail.com>
2010-10-14 18:47:49 -0700

    moving things around to flatten out multiple feeds and sort them together

--- bubbrubb.js
+++ bubbrubb.js
@@ -1,19 +1,41 @@
 (function ($) {
   $.fn.bubbRubb = function (options) {
+    feedData = [];
+
+    function appendData(feed) {
+      $.each(feed, function(i, item) {
+        feedData.push(item);
+      });
+    };
+
+    function displayFeeds() {
+      // TODO: This sort needs to be able to compare the date formats in the feeds
+      //feedData.sort(function(left, right) {
+      //  var a = left.postedTime, b = right.postedTime;
+      //  return a < b ? -1 : a > b ? 1 : 0;
+      //});
+
+      $.each(feedData, function(i, items) {
+        $.each(items, function(i, row){
+          $('#bubbrubb > ul').append("<li class='message'><img class='avatar' src='http://www.gravatar.com/avatar/9ec4d6d86cbf793e1ff04ed4478d95ab.jpg?s=40&d=identicon'/><div class='content'><span class='from'><a class='hover_profile'>" + row.actor.displayName + "</a>	" + row.object.summary + "</span><div class='info'>" + row.postedTime + "</div></div></li>");
+        });
+      });
+    }
+
+
     $('#bubbrubb').append("<ul id='stream'></ul>");
-    $.each(options.feeds, function(i, feed) { 
+    for(var i = 0; i < options.feeds.length; i++) {
       $.ajax({
-        url: feed,
+        url: options.feeds[i],
         dataType: 'jsonp',
         success: function(data) {
           // woooo!
-          $.each(data, function(i, items) {
-            $.each(items, function(i, row){
-              $('#bubbrubb > ul').append("<li class='message'><img class='avatar' src='http://www.gravatar.com/avatar/9ec4d6d86cbf793e1ff04ed4478d95ab.jpg?s=40&d=identicon'/><div class='content'><span class='from'><a class='hover_profile'>" + row.actor.displayName + "</a>	" + row.object.summary + "</span><div class='info'>" + row.postedTime + "</div></div></li>");
-            });
-          });
+          appendData(data);
+          if(i == options.feeds.length) {
+            displayFeeds();
+          }
         }
       });
-    });
+    }
   };
 })(jQuery);
