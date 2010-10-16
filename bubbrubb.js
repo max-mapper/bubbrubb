f110318c50f2d53faf211cfe01ad5a9def256e99
Max Ogden <max@maxogden.com>
2010-10-15 18:09:23 -0700

    use the this, luke

--- bubbrubb.js
+++ bubbrubb.js
@@ -1,6 +1,7 @@
 (function ($) {
   $.fn.bubbRubb = function (options) {
-    feedData = [];
+    var elem = this;
+    var feedData = [];
 
     function appendData(feed) {
       $.each(feed, function(i, item) {
@@ -16,13 +17,13 @@
 
       $.each(feedData, function(i, items) {
         $.each(items, function(i, row){
-          $('#bubbrubb > ul').append("<li class='message'><img class='avatar' src='http://www.gravatar.com/avatar/9ec4d6d86cbf793e1ff04ed4478d95ab.jpg?s=40&d=identicon'/><div class='content'><span class='from'><a href='" + row.object.permalinkUrl + "'class='hover_profile'>" + row.actor.displayName + "</a>	" + row.object.summary + "</span><div class='info'>" + row.postedTime + "</div></div></li>");
+          $(elem).find('.stream').append("<li class='message'><img class='avatar' src='http://www.gravatar.com/avatar/9ec4d6d86cbf793e1ff04ed4478d95ab.jpg?s=40&d=identicon'/><div class='content'><span class='from'><a href='" + row.object.permalinkUrl + "'class='hover_profile'>" + row.actor.displayName + "</a>	" + row.object.summary + "</span><div class='info'>" + row.postedTime + "</div></div></li>");
         });
       });
     }
 
 
-    $('#bubbrubb').append("<ul id='stream'></ul>");
+    $(elem).append("<ul class='stream'></ul>");
     for(var i = 0; i < options.feeds.length; i++) {
       $.ajax({
         url: options.feeds[i],
